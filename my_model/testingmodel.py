f55300c3deb5f800efe94ef0ed8d5a546f3e6cc1
stagingcomms <staging@comms.it>
2023-08-24 15:25:55 +0200

    random numbers update

--- my_model/testingmodel.py
+++ my_model/testingmodel.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import random
 
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-
-cv2.imwrite("first.jpg",x_train[0])
+number = int(random.random()*100)
+cv2.imwrite("first.jpg",x_train[number])
 model = tf.keras.models.load_model('model.h5')
 img = cv2.imread("first.jpg")
 size = [28,28]
@@ -16,7 +17,8 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = (np.expand_dims(img,0))
 print(np.argmax(model(img)))
 
-cv2.imwrite("second.jpg",x_train[5])
+number = int(random.random()*100)
+cv2.imwrite("second.jpg",x_train[number])
 model = tf.keras.models.load_model('model.h5')
 img = cv2.imread("second.jpg")
 size = [28,28]
@@ -25,8 +27,8 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = (np.expand_dims(img,0))
 print(np.argmax(model(img)))
 
-
-cv2.imwrite("third.jpg",x_train[14])
+number = int(random.random()*100)
+cv2.imwrite("third.jpg",x_train[number])
 model = tf.keras.models.load_model('model.h5')
 img = cv2.imread("third.jpg")
 size = [28,28]
